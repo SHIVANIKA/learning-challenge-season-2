e5c35bb9c65f14d087be2bb87bbd65127f79ae90
unknown <shivanika12@gmail.com>
2019-07-02 19:58:21 +0530

    variables and input

---
+++ variables.py
@@ -0,0 +1,68 @@
+BASIC OF PYTHON
+// SIMPLE STRING TO PRINT
+
+print("shivanika")
+print("o----")
+print(" ||||")
+print("*" * 10)
+
+
+//CONCEPT OF VARIABLES
+
+patient_name = "john"
+patient_age = 20
+is_newpatient = True
+print(patient_name)
+
+
+//INPUT
+CHALLENGE TO FIND NAME AND FAVOURITE COLOR
+
+name =  input("what is your name?")
+favourite_color = input("what is your favourite color ?")
+print(name + " likes " + favourite_color)
+
+// CHALLENGE TO FIND AGE USING INPUT
+
+birth_year = input("birth year :")
+print(type(birth_year))
+age =2019 - int (birth_year)
+print(type(age))
+print(age)
+
+//CHALLENGE TO CONVERT POUNDS INTO KILOGARAM
+
+weight = input("what is the weight in pounds ?")
+weight_kilogram = 0.453592 * float(weight) 
+print(weight_kilogram)
+
+
+
+
+OUTPUTS:
+//OUTPUT TO PRINT STRINGS
+
+shivanika
+o----
+ ||||
+**********
+
+//VARIABLES
+john
+
+//OUTPUT OF CHALLENGE TO FIND NAME AND COLOR
+what is your name?shivanika
+what is your favourite color ?blue
+
+//OUTPUT TO FIND AGE 
+shivanika likes blue
+birth year :2000
+<class 'str'>
+<class 'int'>
+19
+
+//OUTPUT TO CONVERT POUNDS TO KILOGRAMS
+what is the weight in pounds ?5
+2.26796
+
+Process finished with exit code 0
