be1850fec735f8dfdfd961cc9fb303414c74479d
unknown <shivanika12@gmail.com>
2019-07-02 20:05:17 +0530

    arthmeticoperations

---
+++ arithmeticoperations.py
@@ -0,0 +1,48 @@
+//ARITHMETIC OPRATIONS
+
+print(10 + 3)
+print(10 // 3)
+print(10%3)
+print(10**3)
+
+
+// OPERATOR PRECEDENCE
+x = 5
+x = x+15
+x -= 3
+x *= 2
+print(x)
+y = 10 +3 * 2**2
+print(y)
+s = (2+3) * 10 -3
+print(s)
+
+
+//MATH FUNCTIONS
+
+import math
+print(math.ceil(6.7))
+print(math.floor(7.3))
+t = 2.9
+print(round(t))
+print(abs(-2.9))
+
+
+//ARITHMETIC OPERATIONS
+13
+3
+1
+1000
+
+//OPERATOR PRECEDENCE
+34
+22
+47
+
+//MATH FUNCTIONS
+7
+7
+3
+2.9
+
+Process finished with exit code 0
